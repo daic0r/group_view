b732ee9b89993436e9259ffd1039c5a8250ccc22
daic0r <daiceman@gmx.net>
2023-03-09 22:34:25 +0100

    Some cleanup and constexpr'ified

--- group_view.cpp
+++ group_view.cpp
@@ -34,21 +34,20 @@ namespace detail
 template<std::size_t N, rg::input_range V>
 requires rg::view<V>
 class group_view : public rg::view_interface<group_view<N, V>> {
-    using container_t = V;
     using T = rg::iterator_t<V>::value_type;
 
 public:
     using difference_type = std::iter_difference_t<std::ranges::iterator_t<V>>;
 
     struct sentinel_t {};
-    static inline constexpr sentinel_t sentinel{};
+    static constexpr inline constexpr sentinel_t sentinel{};
 
     class iterator {
     public: // <-- important
         using difference_type = std::iter_difference_t<std::ranges::iterator_t<V>>;
         using value_type = typename detail::repeat_type<std::add_lvalue_reference_t<std::add_const_t<T>>, N, std::tuple>::type;
         using pointer = value_type*;
-        using reference = value_type&&;
+        using reference = value_type&;
         using iterator_category	= std::input_iterator_tag;
 
     private:
@@ -56,63 +55,63 @@ public:
         rg::iterator_t<V> m_iter{};
 
         template<std::size_t>
-        static decltype(auto) getIterValue(rg::iterator_t<V>& where) noexcept(noexcept(std::declval<rg::iterator_t<V>>()++)) {
+        static constexpr decltype(auto) getIterValue(rg::iterator_t<V>& where) noexcept(noexcept(std::declval<rg::iterator_t<V>>()++)) {
             return *where++;
         }
 
         template<std::size_t... Is>
-        auto fill_tuple(std::index_sequence<Is...>) const noexcept {
+        constexpr auto fill_tuple(std::index_sequence<Is...>) const noexcept {
             auto iter = m_iter;
             return value_type{ getIterValue<Is>(iter)... };
         }
 
     public:
-        iterator() = default;
-        iterator(const V* cont, rg::iterator_t<V> iter) : m_cont{ cont }, m_iter{ iter } {}
-        iterator(const V* cont, sentinel_t) : m_cont{ cont }, m_iter{ std::cend(*m_cont) } {}
+        constexpr iterator() = default;
+        constexpr iterator(const V* cont, rg::iterator_t<V> iter) : m_cont{ cont }, m_iter{ iter } {}
+        constexpr iterator(const V* cont, sentinel_t) : m_cont{ cont }, m_iter{ std::cend(*m_cont) } {}
 
-        value_type operator*() const {  // <-- const important!
+        constexpr value_type operator*() const {  // <-- const important!
             return fill_tuple(std::make_index_sequence<N>{});
         }
 
-        iterator& operator++() {
+        constexpr iterator& operator++() {
             std::advance(m_iter, N);
             return *this;
         }
 
-        iterator operator++(int) const {
+        constexpr iterator operator++(int) const {
             auto ret = *this;
             ++(*this);
             return ret;
         }
 
-        friend bool operator==(iterator const& lhs, iterator const& rhs) noexcept {
+        friend constexpr bool operator==(iterator const& lhs, iterator const& rhs) noexcept {
             return lhs.m_iter == rhs.m_iter;
         }
 
-        friend bool operator!=(iterator const& lhs, iterator const& rhs) noexcept {
+        friend constexpr bool operator!=(iterator const& lhs, iterator const& rhs) noexcept {
             return not(lhs.m_iter == rhs.m_iter);
         }
 
-        bool operator==(sentinel_t) const noexcept {
+        constexpr bool operator==(sentinel_t) const noexcept {
             return m_iter == std::cend(m_cont);
         }
 
-        bool operator!=(sentinel_t) const noexcept {
+        constexpr bool operator!=(sentinel_t) const noexcept {
             return not(m_iter == sentinel);
         }
 
-        bool operator==(rg::iterator_t<V> rhs) const {
+        constexpr bool operator==(rg::iterator_t<V> rhs) const {
             return m_iter == rhs;
         }
     };
 
-    group_view(V cont) : m_base{ std::move(cont) }, begin_{ iterator{ &m_base, std::cbegin(cont) } }, end_{ iterator{ &m_base, std::cend(cont) } } {
+    constexpr group_view(V cont) : m_base{ std::move(cont) }, begin_{ iterator{ &m_base, std::cbegin(cont) } }, end_{ iterator{ &m_base, std::cend(cont) } } {
         static_assert(std::input_iterator<iterator>);
         static_assert(std::sentinel_for<rg::iterator_t<V>, iterator>);
     }
-    auto begin() const { return begin_; }
-    auto end() const { return end_; }
+    constexpr auto begin() const { return begin_; }
+    constexpr auto end() const { return end_; }
 
 private:
     V m_base;
@@ -132,8 +131,6 @@ struct group_view_fn {
 
 template<std::size_t N>
 static inline constexpr auto group = group_view_fn<N>{};
-
-
 
 int main() {
     std::vector<int> v{ 10, 20, 30, 100, 200, 300, 120, 140, 920 };
